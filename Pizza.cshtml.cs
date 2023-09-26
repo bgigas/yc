6caaf31228dccab432a105c26db7d82d9ae05c2f
bgigas <brant@gigas.us>
2023-09-25 18:03:24 -0700

    Create Pizza.cshtml.cs

---
+++ Pizza.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace starting_app.Pages
+{
+  public class PizzaModel : PageModel
+  {
+    public string PizzaTotal (string pizzaType)
+    {
+      Dictionary<string, double> PizzaCost = new Dictionary<string, double>()
+      {
+        { "Cheese", 10 },
+        { "Pepperoni", 11 },
+        { "Vegetarian", 12 },
+        { "PB", 15}
+      };
+      return "$" + String.Format("{0:0.00}", PizzaCost[pizzaType]);
+    }
+
+    public string Total {get; set;}
+    public string Customer {get; set;}
+    public string Order {get; set;}
+    public bool ExtraCheese {get; set;}
+
+    public void OnGet()
+    {
+      Customer = "Brant Gigas";
+      Order = "Cheese";
+      ExtraCheese = false;
+      Total = PizzaTotal("Cheese");
+    }
+  }
+}
