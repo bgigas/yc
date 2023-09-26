8908624a5e4c4d5c2a249f5c8675c6eedb78b634
bgigas <brant@gigas.us>
2023-09-26 07:44:50 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace ProgrammingLanguages
-{
-  class Program
-  {
-    static void Main()
-    {
+namespace ProgrammingLanguages {
+  
+  class Program {
+    
+    static void Main() {
       List<Language> languages = File.ReadAllLines("./languages.tsv")
         .Skip(1)
         .Select(line => Language.FromTsv(line))
